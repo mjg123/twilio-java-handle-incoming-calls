f1083141d295f085d72866ecd99f68dde85a5f96
Matthew Gilliard <matthew@gilliard.lol>
2021-04-20 16:23:51 +0100

    Initial project as downloaded from start.spring.io

---
+++ src/test/java/com/example/twilio/calls/TwilioJavaHandlePhoneCallsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twilio.calls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwilioJavaHandlePhoneCallsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
