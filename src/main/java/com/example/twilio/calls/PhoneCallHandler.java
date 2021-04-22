ffde566025730bf74e423832b87de61da8071173
Matthew Gilliard <matthew@gilliard.lol>
2021-04-22 13:43:30 +0100

    Adds TwiML response using <Say>

--- src/main/java/com/example/twilio/calls/PhoneCallHandler.java
+++ src/main/java/com/example/twilio/calls/PhoneCallHandler.java
@@ -1,5 +1,7 @@
 package com.example.twilio.calls;
 
+import com.twilio.twiml.VoiceResponse;
+import com.twilio.twiml.voice.Say;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PhoneCallHandler {
 
-    @PostMapping("/")
+    @PostMapping(value = "/", produces = "application/xml")
     @ResponseBody
     public String handleIncomingCall(){
-        return "Hello from your app 👋";
+        return new VoiceResponse.Builder()
+            .say(
+                new Say.Builder("Hello from Twilio").build()
+            ).build().toXml();
     }
+
+
 }
